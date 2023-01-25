f29cb17bf01fc20a5630a98a1d83a20fdc4b2484
Gustavo de oliveira <71619573+Gustavoo151@users.noreply.github.com>
2023-01-25 09:02:16 -0300

    01/25

--- Estrutura de dados dinamvicas/listaLigadaSimples.c
+++ Estrutura de dados dinamvicas/listaLigadaSimples.c
@@ -1,8 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef struct No{
+    int num;
+    struct No *proximo;
+}no;
+
+
+void armazena(no *item, no **fim){
+    
+}
 
 int main(){
-    
+    int num;
+    no  *novo =  malloc(sizeof(no));
 
+    if (novo){
+        for (int i = 0; i < 3; i ++){
+            scanf("%d", &num);
+            novo->num = num;
+            novo = novo->proximo;
+        }
+    }
+    else
+        printf("ERRO ao alocar memoria.");
 
     return 0;
 }
