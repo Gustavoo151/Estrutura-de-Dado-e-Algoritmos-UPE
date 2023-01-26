5829a3cf852c356638fd2a2f299d016ec0a2bafb
Gustavo de oliveira <71619573+Gustavoo151@users.noreply.github.com>
2023-01-25 21:02:21 -0300

    Lista ligada simples

--- Estrutura de dados dinamvicas/listaLigadaSimples.c
+++ Estrutura de dados dinamvicas/listaLigadaSimples.c
@@ -1,29 +1,60 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <stdio.h>  // Biblioteca de que serve para fezer os inputs e output
+#include <stdlib.h>  // Biblioteca usada para fazer a alocação dinamica
 
-typedef struct No{
+typedef struct No{  // Criando estrutura para fazer a ligação dos nós
     int num;
-    struct No *proximo;
+    struct No *proximo;  // No que liga no proximo no
 }no;
 
 
-void armazena(no *item, no **fim){
-    
+no* listaligadaSimples(int  num, no *listaSimples){  // Recebendo o numero como primeiro parametro e um ponteiro para um No nulo que vair ser o no final da estrutura
+    no *ligar = malloc(sizeof(no));  // Alocando a memória necessária de cada No
+
+    if(ligar){  // Verificando se a memoria foi alocada corretamente
+        ligar->num = num;  // passando o valor de parametro num
+        ligar->proximo = listaSimples; // Ligando o no atual ao nos anteriores
+        return ligar;  // Retornando o primeiro No
+    }
+    else  // Se a memoria não foi alocada
+        printf("Erro ao alocar memoria.");
+    return NULL;  // Caso a memoria no seja alocada retorna NULL
 }
 
-int main(){
-    int num;
-    no  *novo =  malloc(sizeof(no));
 
-    if (novo){
-        for (int i = 0; i < 3; i ++){
-            scanf("%d", &num);
-            novo->num = num;
-            novo = novo->proximo;
-        }
+void imprimir(no *lista){  // Recebe o ponteiro para o primeiro item da lista
+
+    while(lista){  // Equanto o ponteiro não for nulo 
+        printf("%d \n", lista->num);  // Imprimindo o valor atribuido ao no
+        lista = lista->proximo;  // Tornando o No atual ser o próximo No
     }
-    else
-        printf("ERRO ao alocar memoria.");
+}
+
+
+int main(){
+    int num, opcao = 1;
+    no  *lista = NULL;  // No nulo que vai ser o último No da lista ligada 
+
+    do {
+        printf("1 - Para adcionar um novo valor\n2 - Para imprimir\n0 - sair\n");
+        scanf("%d", &opcao);
+
+        switch (opcao){
+            case 1:
+                printf("Digite um valor para ser adicionado na lista\n");
+                scanf("%d", &num);
+                lista = listaligadaSimples(num, lista);
+                break;
+            case 2:
+                printf("Imprimindo lista:\n");
+                imprimir(lista); 
+                printf("\n\n");
+                break;        
+            case 0:
+                return 0;
+            default:
+                printf("Valor inválido.\n");
+        }
+    }while (opcao != 0);
 
     return 0;
 }
