4ab34f9a962bd5b1d332367b8ba3758f13f94a82
Gustavo de oliveira <71619573+Gustavoo151@users.noreply.github.com>
2023-03-06 00:39:33 -0300

    Update 02.c

--- 1º Trabalho de Implementação/02.c
+++ 1º Trabalho de Implementação/02.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct no{
+typedef struct no{   // Estrutura para fazer o encademento do Nos e salvar um valor inteiro
     int dado;
     struct tPilhaSimples *proximo;
 }tPilhaSimples;
 
 
-tPilhaSimples* CriaPilha(int novo){
+tPilhaSimples* CriaPilha(int novo){  // Função que cria Pilha
     tPilhaSimples *topo;
 
-    topo = malloc(sizeof(tPilhaSimples));
+    topo = malloc(sizeof(tPilhaSimples));  // Faz a alocação de memoria 
     topo->dado = novo;
     topo->proximo = NULL;
 
@@ -18,18 +18,18 @@ tPilhaSimples* CriaPilha(int novo){
 }
 
 
-int PilhaVazia(tPilhaSimples *topo){
-    if (topo == NULL)
+int PilhaVazia(tPilhaSimples *topo){  // Função que verifica se a pilha está vazia
+    if (topo == NULL) 
         return 1;
     else
         return 0;
 }
 
 
-tPilhaSimples* Empilha(tPilhaSimples *topo, int novo){
+tPilhaSimples* Empilha(tPilhaSimples *topo, int novo){  // Função que empilha os NOS
     tPilhaSimples *novoTopo;
 
-    novoTopo = malloc(sizeof(tPilhaSimples));
+    novoTopo = malloc(sizeof(tPilhaSimples));  // Alocando memória
 
     if(novoTopo == NULL)
         return topo;
@@ -37,11 +37,11 @@ tPilhaSimples* Empilha(tPilhaSimples *topo, int novo){
     novoTopo->dado = novo;
     novoTopo->proximo = topo;
 
-    return novoTopo;
+    return novoTopo;  // Retorna o nó que é o novo topo da pilha
 }
 
 
-tPilhaSimples* Desempilha(tPilhaSimples *topo, int *quantRemovidos){
+tPilhaSimples* Desempilha(tPilhaSimples *topo, int *quantRemovidos){  // Função que desempilha um caso
     tPilhaSimples *novoTopo;
 
     if(topo == NULL){
@@ -53,20 +53,20 @@ tPilhaSimples* Desempilha(tPilhaSimples *topo, int *quantRemovidos){
 
     novoTopo = topo->proximo;
     //*(quantRemovidos++);
-    printf("\nQuantidade de intens removidos: %d\n", *quantRemovidos+= 1);
+    printf("\nQuantidade de intens removidos: %d\n", *quantRemovidos+= 1);  // Adiciona mais 1 a cada nó removido
     
-    free(topo);
+    free(topo);   // Liberando memória
     return novoTopo;
 } 
 
 
-void mostraPilha(tPilhaSimples *topo){
+void mostraPilha(tPilhaSimples *topo){   // Função para imprimir toda pilha
     printf("\n------------------- PILHA -------------------\n");
 
     while(topo != NULL)
     {
         printf("%d\n", topo->dado);
-        topo = topo->proximo;
+        topo = topo->proximo;  // Topo recebe próximo para acessar os dados do próximo e o programa não ficar em loop infinito
     }
     printf("\n----------------- FIM PILHA -----------------\n");
 }
@@ -76,11 +76,11 @@ int main(){
     int opc, valor, *quantRemovidos = 0;
     tPilhaSimples *topo = NULL;
 
-    do {
+    do {  // Lool infinito até o usuário digitar 0
         printf("\n1 - Teste Pilha Vazia\n2 - Cria Pilha\n3 - Empilhar\n4 - Desemplihar\n5 - Mostrar Elementos\n0 - Sair\n");
         scanf("%d", &opc);
 
-        switch (opc)
+        switch (opc)   // Switch case para controlar as opções do menu
         {
         case 1:
             if(PilhaVazia(topo) == 1)
